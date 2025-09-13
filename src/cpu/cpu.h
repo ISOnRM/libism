97286ad9ea33e963ec670dde68f91334cfc2555b
mark <markison7002@gmail.com>
2025-09-14 01:28:59 +0300

    Add cpu/cpu.h, add */placeholder(s) for other directories

---
+++ src/cpu/cpu.h
@@ -0,0 +1,14 @@
+#ifndef CPU_H
+#define CPU_H
+
+typedef unsigned long size_t;
+
+typedef enum { CPU_PCT_INT, CPU_PCT_FLOAT } cpu_pct_fmt_t;
+
+int get_cpu_pct(char *buf, size_t bufsiz, cpu_pct_fmt_t pct_fmt);
+
+int get_cpu_freq(char *buf, size_t bufsiz);
+
+// int get_cpu_topology
+
+#endif
