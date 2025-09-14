b65e6f9577c5d42a56228837c697465099dc63a4
mark <markison7002@gmail.com>
2025-09-15 00:20:33 +0300

    Rename ram/ to mem/, add mem.h and mem.c to mem/

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -4,13 +4,12 @@
 #ifndef CPU_H
 #define CPU_H
 
+#include <stddef.h>
 #include "util/pct_fmt.h"
-
-typedef unsigned long size_t;
 
 int get_cpu_pct(char *buf, size_t bufsiz, double interval, pct_fmt_t pct_fmt);
 
-int get_cpu_freq(char *buf, size_t bufsiz);
+// int get_cpu_freq(char *buf, size_t bufsiz);
 
 // int get_cpu_topology
 
---
+++ src/mem/mem.h
@@ -0,0 +1,28 @@
+#ifndef MEM_H
+#define MEM_H
+
+#include <stddef.h>
+#include "util/pct_fmt.h"
+
+typedef enum { RAM, SWAP } mem_type_t;
+typedef enum { TOTAL, FREE, USED } mem_fld_t;
+typedef enum { FREE_PCT, USED_PTC } mem_pct_fld_t;
+
+// Write field of memory type to buffer (human readable)
+int get_mem(
+    char *buf,
+    size_t bufsiz,
+    mem_type_t mem_type,
+    mem_fld_t mem_fld
+);
+
+// Write memory type's filed's percent to buffer
+int get_mem_pct(
+    char *buf,
+    size_t bufsiz,
+    mem_type_t mem_type,
+    mem_pct_fld_t mem_pct_fld,
+    pct_fmt_t pct_fmt
+);
+
+#endif
